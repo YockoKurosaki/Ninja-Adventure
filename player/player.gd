58f5b29f11509cbcdd0f950ea6b152f85ac6177d
María Torres <mtorres@mrhouston.net>
2023-10-21 22:49:46 +0200

    flowers

--- player/player.gd
+++ player/player.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 # The encapsulate "export" allows me to edit this variable also from the inspector
 @export var speed: int = 40 
+@onready var animations = $AnimationPlayer
 
 
 func handleInput():
@@ -10,9 +11,24 @@ func handleInput():
 	# 2. Set velocity to movement direction * speed
 	velocity = moveDirection*speed
 	
+func updateAnimation():
+	# If is not moving
+	if velocity.length() == 0:
+		if animations.is_playing():
+			animations.stop()
+	else:
+		var direction = "down"
+		# Validate in which direction is moving the character to select the correct animation
+		if velocity.x < 0: direction = "left"
+		elif velocity.x > 0: direction = "right"
+		elif velocity.y < 0: direction = "up"
+		animations.play("walk_" + direction)
+	
+
 # This is a built in function like the Update in Unity but also related to physics
 func _physics_process(delta):
 	handleInput()
 	# 3. Move player
 	move_and_slide() #This is a built in function that the tutorial decided to use instead move_and_collide
+	updateAnimation()
 
